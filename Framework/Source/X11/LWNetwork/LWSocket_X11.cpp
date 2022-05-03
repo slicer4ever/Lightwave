bf423257a35036e417bd14c7b55694f46d42f06c
T Collins <slicer4ever06@aim.com>
2022-05-03 09:24:39 -0400

    Updated linux socket implementation.

--- Framework/Source/X11/LWNetwork/LWSocket_X11.cpp
+++ Framework/Source/X11/LWNetwork/LWSocket_X11.cpp
@@ -7,13 +7,24 @@
 #include <resolv.h>
 #include <iostream>
 
-uint32_t LWSocket::LookUpAddress(const LWUTF8Iterator &Address, uint32_t *IPBuffer, uint32_t IPBufferLen, char *Addresses, uint32_t AddressLen) {
-	addrinfo hint = { AI_CANONNAME, AF_INET, 0, 0, 0, nullptr, nullptr, nullptr }, *servinfo = nullptr;
+sockaddr_in ToSockAddr(const LWSocketAddr &Addr) {
+	sockaddr_in Result = { AF_INET, LWByteBuffer::MakeNetwork(Addr.GetPort()), 0, {0} };
+	Result.sin_addr.S_un.S_addr = LWByteBuffer::MakeNetwork(Addr.IP[3]);
+	return Result;
+}
+
+LWSocketAddr FromSockAddr(const sockaddr_in &Addr) {
+	return LWSocketAddr(LWByteBuffer::MakeHost((uint32_t)Addr.sin_addr.S_un.S_addr), LWByteBuffer::MakeHost(Addr.sin_port));
+}
+
+
+uint32_t LWSocket::LookUpAddress(const LWUTF8Iterator &Address, LWSocketAddr *AddrBuffer, uint32_t AddrBufferLen, char *Addresses, uint32_t AddressLen) {
+	addrinfo hint = { AI_CANONNAME | AI_RETURN_PREFERRED_NAMES, AF_INET, 0, 0, 0, nullptr, nullptr, nullptr }, *servinfo = nullptr;
 	if (getaddrinfo(*Address.c_str<256>(), nullptr, &hint, &servinfo)) return 0xFFFFFFFF;
 	unsigned int i = 0;
 	char *AL = Addresses + AddressLen;
 	for (addrinfo *p = servinfo; p; p = p->ai_next, i++) {
-		if (i < IPBufferLen) IPBuffer[i] = LWByteBuffer::MakeHost((uint32_t)((sockaddr_in*)p->ai_addr)->sin_addr.s_addr);
+		if (i < AddrBufferLen) AddrBuffer[i] = FromSockAddr(*(sockaddr_in *)p->ai_addr);
 		if (p->ai_canonname) {
 			char *c = p->ai_canonname;
 			for (; Addresses != AL && c; Addresses++, c++) *Addresses = *c;
@@ -31,143 +42,101 @@ uint32_t LWSocket::LookUpAddress(const LWUTF8Iterator &Address, uint32_t *IPBuff
 	return i;
 }
 
-uint32_t LWSocket::LookUpDNSServers(uint32_t *IPBuf, uint32_t IPBufferLen) {
+uint32_t LWSocket::LookUpDNSServers(LWSocketAddr *AddrBuffer, uint32_t AddrBufferLen) {
 	res_init();
 	uint32_t Cnt = 0;
 	for (; Cnt < _res.nscount; Cnt++) {
 		auto addr = _res.nsaddr_list[Cnt];
-		if (Cnt < IPBufferLen) {
-			IPBuf[Cnt] = LWByteBuffer::MakeHost((uint32_t)((sockaddr_in*)&addr)->sin_addr.s_addr);
-		}
+		if (Cnt < AddrBufferLen) AddrBuffer[Cnt] = FromSockAddr(*(sockaddr_in*)&addr);
 	}
 	return Cnt;
 }
 
+uint32_t LWSocket::CreateSocket(LWSocket &Socket, uint32_t Type, const LWSocketAddr &RemoteAddr, uint16_t LocalPort, uint32_t Flag, uint32_t ProtocolID) {
+	sockaddr_in sBindAddr = ToSockAddr(LWSocketAddr(0, LocalPort));
+	sockaddr_in sConnectAddr = ToSockAddr(RemoteAddr);
+	int32_t AddrSize = sizeof(sockaddr_in);
+	uint32_t UPDNoConnReset = (Flag & LWSocket::UDPConnReset) == 0;
+	uint32_t SockBroadcast = (Flag & LWSocket::BroadcastAble) != 0;
+	uint32_t iTcpNoDelay = (Flag & LWSocket::TcpNoDelay) != 0;
+	uint32_t iReuseAddr = (Flag & LWSocket::ReuseAddr) != 0;
 
-uint32_t LWSocket::CreateSocket(LWSocket &Socket, uint16_t Port, uint32_t Flag, uint32_t ProtocolID) {
-	sockaddr_in Addr = { AF_INET, LWByteBuffer::MakeNetwork(Port), INADDR_ANY,{ 0 } }, lAddr;
-	socklen_t AddrLen = sizeof(Addr);
-	uint32_t UPDNoConnReset = (Flag&LWSocket::UdpConnReset) ? true : false;
-	uint32_t SockBroadcast = (Flag&LWSocket::Broadcast) ? true : false;
-	uint32_t TcpNoDelay = (Flag&LWSocket::TcpNoDelay) ? true : false;
-	uint32_t ReuseAddr = (Flag&LWSocket::ReuseAddr) ? true : false;
-	uint32_t SockID = (uint32_t)socket(AF_INET, Flag&LWSocket::Udp ? SOCK_DGRAM : SOCK_STREAM, Flag&LWSocket::Udp ? IPPROTO_UDP : IPPROTO_TCP);
+	LWSocketHandle Handle = socket(AF_INET, Type == LWSocket::UDP ? SOCK_DGRAM : SOCK_STREAM, Type == LWSocket::UDP ? IPPROTO_UDP : IPPROTO_TCP);
 
-	if (SockID == INVALID_SOCKET) return LWSocket::ErrSocket;
-	if (Flag&LWSocket::Udp) {
-		/*int32_t r = ioctl(SockID, UDP_CONNRESET, (u_long*)&UPDNoConnReset);
-		if (r) {
-			return LWSocket::ErrCtrlFlags;
-		}*/
-		if (setsockopt(SockID, SOL_SOCKET, SO_BROADCAST, (char*)&SockBroadcast, sizeof(SockBroadcast))) return LWSocket::ErrCtrlFlags;
+	if (Handle == INVALID_SOCKET) return LWSocket::E_Socket;
+	if (Type == LWSocket::UDP) {
+		if (ioctlsocket(Handle, (IOC_IN | IOC_VENDOR | 0xC), (u_long *)&UPDNoConnReset)) return LWSocket::E_CtrlFlags;
+		if (setsockopt(Handle, SOL_SOCKET, SO_BROADCAST, (char *)&SockBroadcast, sizeof(SockBroadcast))) return LWSocket::E_CtrlFlags;
 	} else {
-		if (setsockopt(SockID, IPPROTO_TCP, TCP_NODELAY, (char*)&TcpNoDelay, sizeof(TcpNoDelay))) return LWSocket::ErrCtrlFlags;
+		if (setsockopt(Handle, IPPROTO_TCP, TCP_NODELAY, (char *)&iTcpNoDelay, sizeof(iTcpNoDelay))) return LWSocket::E_CtrlFlags;
 	}
 
-	if (setsockopt(SockID, SOL_SOCKET, SO_REUSEADDR, (char*)&ReuseAddr, sizeof(ReuseAddr))) return LWSocket::ErrCtrlFlags;
-	if (bind(SockID, (sockaddr*)&Addr, AddrLen)) return LWSocket::ErrBind;
-	if (Flag&LWSocket::Listen) if (listen(SockID, LWSocket::MaxBacklog)) return LWSocket::ErrListen;
-	if (getsockname(SockID, (sockaddr*)&lAddr, &AddrLen)) return LWSocket::ErrGetSock;
+	if (setsockopt(Handle, SOL_SOCKET, SO_REUSEADDR, (char *)&iReuseAddr, sizeof(iReuseAddr))) return LWSocket::E_CtrlFlags;
+	if (bind(Handle, (sockaddr *)&sBindAddr, AddrSize)) return LWSocket::E_Bind;
+	if (RemoteAddr.IsValid()) {
+		if (connect(Handle, (sockaddr *)&sConnectAddr, AddrSize)) return LWSocket::E_Connect;
+		if (getpeername(Handle, (sockaddr *)&sConnectAddr, &AddrSize)) return LWSocket::E_GetPeer;
+	}
+	if (Flag & LWSocket::Listening) {
+		if (listen(Handle, DefaultBacklog)) return LWSocket::E_Listen;
+	}
+	if (getsockname(Handle, (sockaddr *)&sBindAddr, &AddrSize)) return LWSocket::E_GetSock;
 
-	Socket = LWSocket(SockID, ProtocolID, LWByteBuffer::MakeHost((uint32_t)lAddr.sin_addr.s_addr), LWByteBuffer::MakeHost(lAddr.sin_port), 0, 0, Flag);
+	Socket = LWSocket(Handle, FromSockAddr(sBindAddr), FromSockAddr(sConnectAddr), Flag | Type, ProtocolID);
 	return 0;
 }
 
-uint32_t LWSocket::CreateSocket(LWSocket &Socket, uint32_t RemoteIP, uint16_t RemotePort, uint16_t LocalPort, uint32_t Flag, uint32_t ProtocolID) {
-	sockaddr_in lAddr = { AF_INET, LWByteBuffer::MakeNetwork(LocalPort), INADDR_ANY,{ 0 } };
-	sockaddr_in rAddr = { AF_INET, LWByteBuffer::MakeNetwork(RemotePort), 0,{ 0 } };
-	rAddr.sin_addr.s_addr = LWByteBuffer::MakeNetwork(RemoteIP);
-	socklen_t lAddrLen = sizeof(lAddr);
-	socklen_t rAddrLen = sizeof(rAddr);
-	uint32_t UPDNoConnReset = (Flag&LWSocket::UdpConnReset) ? true : false;
-	uint32_t SockBroadcast = (Flag&LWSocket::Broadcast) ? true : false;
-	uint32_t TcpNoDelay = (Flag&LWSocket::TcpNoDelay) ? true : false;
-	uint32_t ReuseAddr = (Flag&LWSocket::ReuseAddr) ? true : false;
 
-	uint32_t SockID = (uint32_t)socket(AF_INET, Flag&LWSocket::Udp ? SOCK_DGRAM : SOCK_STREAM, Flag&LWSocket::Udp ? IPPROTO_UDP : IPPROTO_TCP);
-
-	if (SockID == INVALID_SOCKET) return LWSocket::ErrSocket;
-	if (Flag&LWSocket::Udp) {
-		//if (ioctl(SockID, UDP_CONNRESET, (u_long*)&UPDNoConnReset)) return LWSocket::ErrCtrlFlags;
-		if (setsockopt(SockID, SOL_SOCKET, SO_BROADCAST, (char*)&SockBroadcast, sizeof(SockBroadcast))) return LWSocket::ErrCtrlFlags;
-	} else {
-		if (setsockopt(SockID, IPPROTO_TCP, TCP_NODELAY, (char*)&TcpNoDelay, sizeof(TcpNoDelay))) return LWSocket::ErrCtrlFlags;
-	}
-
-	if (setsockopt(SockID, SOL_SOCKET, SO_REUSEADDR, (char*)&ReuseAddr, sizeof(ReuseAddr))) return LWSocket::ErrCtrlFlags;
-	if (bind(SockID, (sockaddr*)&lAddr, lAddrLen)) return LWSocket::ErrBind;
-	if (connect(SockID, (sockaddr*)&rAddr, rAddrLen)) return LWSocket::ErrConnect;
-	if (Flag&LWSocket::Listen) if (listen(SockID, LWSocket::MaxBacklog)) return LWSocket::ErrListen;
-	if (getsockname(SockID, (sockaddr*)&lAddr, &lAddrLen)) return LWSocket::ErrGetSock;
-	if (getpeername(SockID, (sockaddr*)&rAddr, &rAddrLen)) return LWSocket::ErrGetPeer;
-
-	Socket = LWSocket(SockID, ProtocolID, LWByteBuffer::MakeHost((uint32_t)lAddr.sin_addr.s_addr), LWByteBuffer::MakeHost(lAddr.sin_port), LWByteBuffer::MakeHost((uint32_t)rAddr.sin_addr.s_addr), LWByteBuffer::MakeHost(rAddr.sin_port), Flag);
-	return 0;
-}
-
-uint32_t LWSocket::CreateSocket(LWSocket &Socket, const LWUTF8Iterator &Address, uint16_t RemotePort, uint16_t LocalPort, uint32_t Flag, uint32_t ProtocolID) {
-	uint32_t AddrIP = 0;
-	addrinfo Hint = { 0, AF_INET, 0, 0, 0, nullptr, nullptr, nullptr }, *Result;
-	if (getaddrinfo(*Address.c_str<256>(), nullptr, &Hint, &Result)) return LWSocket::ErrAddress;
-	AddrIP = LWByteBuffer::MakeHost((uint32_t)((sockaddr_in*)Result->ai_addr)->sin_addr.s_addr);
-	freeaddrinfo(Result);
-	return CreateSocket(Socket, AddrIP, RemotePort, LocalPort, (Flag | LWSocket::Tcp)&~LWSocket::Udp, ProtocolID);
-}
-
-uint32_t LWSocket::Receive(char *Buffer, uint32_t BufferLen) {
-	uint32_t Len = recv(m_SocketID, Buffer, BufferLen, 0);
+uint32_t LWSocket::Receive(void *Buffer, uint32_t BufferLen) {
+	uint32_t Len = recv(m_Handle, (char *)Buffer, BufferLen, 0);
 	if (Len != -1) m_RecvBytes += Len;
 	return Len;
 }
 
-uint32_t LWSocket::Receive(char *Buffer, uint32_t BufferLen, uint32_t *RemoteIP, uint16_t *RemotePort) {
+uint32_t LWSocket::Receive(void *Buffer, uint32_t BufferLen, LWSocketAddr &RemoteAddr) {
 	sockaddr_in rAddr;
-	socklen_t rAddrLen = sizeof(rAddr);
-	uint32_t Len = recvfrom(m_SocketID, Buffer, BufferLen, 0, (sockaddr*)&rAddr, &rAddrLen);
-	if (RemoteIP) *RemoteIP = LWByteBuffer::MakeHost((uint32_t)rAddr.sin_addr.s_addr);
-	if (RemotePort) *RemotePort = LWByteBuffer::MakeHost(rAddr.sin_port);
+	int32_t rAddrLen = sizeof(rAddr);
+	uint32_t Len = recvfrom(m_Handle, (char *)Buffer, BufferLen, 0, (sockaddr *)&rAddr, &rAddrLen);
+	RemoteAddr = FromSockAddr(rAddr);
 	if (Len != -1) m_RecvBytes += Len;
 	return Len;
 }
 
-uint32_t LWSocket::Send(const char *Buffer, uint32_t BufferLen) {
-	uint32_t Len = send(m_SocketID, Buffer, BufferLen, 0);
+uint32_t LWSocket::Send(const void *Buffer, uint32_t BufferLen) {
+	uint32_t Len = send(m_Handle, (const char *)Buffer, BufferLen, 0);
 	if (Len != -1) m_SentBytes += Len;
 	return Len;
 }
 
-uint32_t LWSocket::Send(char *Buffer, uint32_t BufferLen, uint32_t RemoteIP, uint16_t RemotePort) {
-	sockaddr_in rAddr = { AF_INET, LWByteBuffer::MakeNetwork(RemotePort), 0,{ 0 } };
-	rAddr.sin_addr.s_addr = LWByteBuffer::MakeNetwork(RemoteIP);
-	uint32_t Len = sendto(m_SocketID, Buffer, BufferLen, 0, (sockaddr*)&rAddr, sizeof(rAddr));
+uint32_t LWSocket::Send(const void *Buffer, uint32_t BufferLen, const LWSocketAddr &RemoteAddr) {
+	sockaddr_in rAddr = ToSockAddr(RemoteAddr);
+	uint32_t Len = sendto(m_Handle, (const char *)Buffer, BufferLen, 0, (sockaddr *)&rAddr, sizeof(rAddr));
 	if (Len != -1) m_SentBytes += Len;
 	return Len;
 }
 
 bool LWSocket::Accept(LWSocket &Result, uint32_t ProtocolID) const {
 	sockaddr_in rAddr, lAddr;
-	socklen_t rAddrLen = sizeof(rAddr);
-	socklen_t lAddrLen = sizeof(lAddr);
-	uint32_t SockID = (uint32_t)accept(m_SocketID, nullptr, nullptr);
+	int32_t rAddrLen = sizeof(rAddr);
+	int32_t lAddrLen = sizeof(lAddr);
+	uint32_t SockID = (uint32_t)accept(m_Handle, nullptr, nullptr);
 
-	uint32_t TcpNoDelay = (m_Flags&LWSocket::TcpNoDelay) ? true : false;
-	if (setsockopt(SockID, IPPROTO_TCP, TCP_NODELAY, (char*)&TcpNoDelay, sizeof(TcpNoDelay))) return false;
+	uint32_t TcpNoDelay = (m_Flags & LWSocket::TcpNoDelay) ? true : false;
+	if (setsockopt(SockID, IPPROTO_TCP, TCP_NODELAY, (char *)&TcpNoDelay, sizeof(TcpNoDelay))) return false;
 
-	if (getsockname(SockID, (sockaddr*)&lAddr, &lAddrLen)) return false;
-	if (getpeername(SockID, (sockaddr*)&rAddr, &rAddrLen)) return false;
-	Result = LWSocket(SockID, ProtocolID, LWByteBuffer::MakeHost((uint32_t)lAddr.sin_addr.s_addr), LWByteBuffer::MakeHost(lAddr.sin_port), LWByteBuffer::MakeHost((uint32_t)rAddr.sin_addr.s_addr), LWByteBuffer::MakeHost(rAddr.sin_port), m_Flags&~LWSocket::Listen);
+	if (getsockname(SockID, (sockaddr *)&lAddr, &lAddrLen)) return false;
+	if (getpeername(SockID, (sockaddr *)&rAddr, &rAddrLen)) return false;
+	Result = LWSocket(SockID, FromSockAddr(lAddr), FromSockAddr(rAddr), m_Flags & ~LWSocket::Listening, ProtocolID);
 	return true;
 }
 
 LWSocket &LWSocket::Close(void) {
-	if (m_SocketID) {
-		m_Flags |= LWSocket::Closeable;
-		close(m_SocketID);
-		m_SocketID = 0;
-	}
+	m_Flags = LWBitFieldSet(StatusBits, m_Flags, S_Closed);
+	if (m_Handle) closesocket(m_Handle);
+	m_Handle = 0;
 	return *this;
 }
 
 LWSocket::~LWSocket() {
-	if (m_SocketID) close(m_SocketID);
+	Close();
 }
